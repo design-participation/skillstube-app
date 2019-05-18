fbf0ddb092d12096f1934490415ba74bc7722720
Benoit Favre <benoit.favre@lis-lab.fr>
2019-05-18 12:35:09 +1000

    reload SSL certificate every day

--- src/server.py
+++ src/server.py
@@ -27,7 +27,21 @@ if '-debug' in sys.argv[1:]:
 import secrets
 from util import routes, get_user, add_globals, error_middleware
 
-async def init():
+ssl_context = None
+if secrets.USE_SSL:
+    import ssl
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(secrets.SSL_CRT, secrets.SSL_KEY)
+
+async def update_certificate():
+    while True:
+        # let's encrypt certificate is updated every 3 months, we need to reload it
+        # TODO: only do it if certificate changed
+        print('reloading SSL certificate')
+        ssl_context.load_cert_chain(secrets.SSL_CRT, secrets.SSL_KEY)
+        await asyncio.sleep(3600 * 24) # once a day
+
+async def run_web_app():
     app = web.Application(middlewares=[error_middleware])
 
     setup(app, EncryptedCookieStorage(secrets.SERVER_COOKIE_KEY))
@@ -38,15 +52,10 @@ async def init():
     routes.static('/pictures', 'data/pictures')
     routes.static('/', 'static/favicon')
     app.add_routes(routes) 
+
+    if secrets.USE_SSL:
+        asyncio.get_event_loop().create_task(update_certificate())
     return app
 
-app = asyncio.get_event_loop().run_until_complete(init())
-
-ssl_context = None
-if secrets.USE_SSL:
-    import ssl
-    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-    ssl_context.load_cert_chain(secrets.SSL_CRT, secrets.SSL_KEY)
-
+app = asyncio.get_event_loop().run_until_complete(run_web_app())
 web.run_app(app, ssl_context=ssl_context, host=secrets.HOST, port=secrets.PORT)
-
