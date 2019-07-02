96181ecc51f1c0836bdba6d215415263a2dd8434
Benoit Favre <benoit.favre@lis-lab.fr>
2019-07-02 11:02:20 +1000

    improve qrcode scanner compatibility

--- static/qrcode_scanner.js
+++ static/qrcode_scanner.js
@@ -1,20 +1,12 @@
+var videoDevices = [];
 
 function init_qrcode_scanner(error_callback) {
-	window.navigator = window.navigator || {};
-	navigator.getUserMedia = navigator.getUserMedia       ||
-		navigator.webkitGetUserMedia ||
-		navigator.mozGetUserMedia    ||
-		null;
-	if(navigator.getUserMedia === null) {
-		console.log('getUserMedia unsupported');
-		error_callback();
-	}
 }
 
 var videoStream = null;
 var interval;
 
-function run_qrcode_scanner(result_callback, camera_type) {
+function run_qrcode_scanner(result_callback, camera_num) {
 	console.log('start qrcode scanner');
 	qrcode.callback = function(result) {
 		console.log('scan result [' + result + ']');
@@ -37,29 +29,40 @@ function run_qrcode_scanner(result_callback, camera_type) {
 		clearInterval(interval);
 	}
 
-	// get new stream
-	let constraints = {video: {width: {exact: 320}, height: {exact: 240}, facingMode: {exact: (camera_type == 1 ? 'user' : 'environment')} }};
-	console.log(constraints);
-	navigator.getUserMedia(constraints , function(stream) {
-		videoStream = stream;
-		video.srcObject = stream;
-		video.play();
-		interval = setInterval(function() {
-			canvas.width = video.videoWidth;
-			canvas.height = video.videoHeight;
-			context.drawImage(video, 0, 0);
-			try {
-				qrcode.decode();
-			} catch(error) {
-				//console.log(error);
-			}
-		}, 250)
-
-	}, function(error) {
-		console.log('Video capture error: ', error.code);
-		console.log($('#choose-camera'));
-		$('#choose-camera').popover('dispose').popover({html: true, content: '<i style="color: var(--danger)" class="fas fa-times-circle"></i> Did you allow video recording?', placement: 'top'}).popover('show');
-	});
+  // update list of devices
+  navigator.mediaDevices.enumerateDevices()
+    .then(function(devices) {
+      videoDevices = [];
+      devices.forEach(function(device) {
+        if(device.kind == 'videoinput') {
+          console.log(device);
+          videoDevices.push(device);
+        }
+      });
+      if(videoDevices.length == 0) throw "No video device found";
+      let device = videoDevices[camera_num % videoDevices.length];
+      let constraints = {video: {width: 320, height: 240, deviceId: device.deviceId}};
+      return navigator.mediaDevices.getUserMedia(constraints);
+    })
+    .then(function(stream) {
+      videoStream = stream;
+      video.srcObject = stream;
+      video.play();
+      interval = setInterval(function() {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        context.drawImage(video, 0, 0);
+        try {
+          qrcode.decode();
+        } catch(error) {
+          //console.log(error);
+        }
+      }, 250)
+    }).catch(function(error) {
+      console.log('Video capture error: ', error);
+      console.log($('#choose-camera'));
+      $('#choose-camera').popover('dispose').popover({html: true, content: '<i style="color: var(--danger)" class="fas fa-times-circle"></i> Did you allow video recording?', placement: 'top'}).popover('show');
+    });
 }
 
 function stop_qrcode_scanner() {
