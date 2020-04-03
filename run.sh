f13269b38e0cd89f5ef8b863f49729fcf513ed09
Benoit Favre <benoit.favre@lis-lab.fr>
2020-04-03 11:14:48 +0200

    update requirements and mongodb command-line

--- run.sh
+++ run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-PGID=$(ps -o pgid= $PID | grep -o [0-9]*)
-
-trap "kill -TERM -$PGID" SIGTERM
+trap 'kill -TERM $(jobs -p)' SIGTERM
 
 timestamp=`date '+%Y-%m-%d_%H:%M:%S'`
 mkdir -p data/mongo data/pictures data/qrcodes data/export data/logs
@@ -16,8 +14,8 @@ if [ ! -r src/secrets.py ]; then
 fi
 
 # warn: remove disk-saving options in production (only keep --dbpath)
-mongod --nojournal --nssize=1 --noprealloc --smallfiles --dbpath data/mongo --port `grep DB_URL src/secrets.py | cut -f2 -d"'" | awk -F: '{print $(NF)}'` > "data/logs/db_$timestamp.txt" 2>&1 &
+mongod --dbpath data/mongo --port `grep DB_URL src/secrets.py | cut -f2 -d"'" | awk -F: '{print $(NF)}'` > "data/logs/db_$timestamp.txt" 2>&1 &
 
 python -u src/server.py $* 2>&1 | tee "data/logs/server_$timestamp.txt"
 
-kill -TERM -$PGID
+kill -TERM $(jobs -p)
